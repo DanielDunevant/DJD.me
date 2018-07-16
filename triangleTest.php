df355c79b7fc944f284a5d3028696d3fba3d4990
DanielDunevant <daniel.john.dunevant@gmail.com>
2018-07-16 08:28:55 -0500

    Triangle and Hexagon Divs

--- triangleTest.php
+++ triangleTest.php
@@ -1,45 +1,74 @@
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <style>
-              .arrow-up {
-                  width: 0; 
-                  height: 0; 
-                  border-left: 5px solid transparent;
-                  border-right: 5px solid transparent; 
-                  border-bottom: 5px solid black;
-               }
+	<head>
+        	<style>
+              		.arrow-up {
+                  		width:  0; 
+		  		height: 0; 
+		  		position:relative;
+		  		display:inline-block;
+                  		border-left: 320px solid transparent;
+                  		border-right: 320px solid transparent; 
+                  		border-bottom: 560px solid black;
+               		}
 
-              .arrow-down {
-                  width: 0; 
-                  height: 0; 
-                  border-left: 20px solid transparent;
-                  border-right: 20px solid transparent;
-                  border-top: 20px solid #f00;
-              }
+              		.arrow-down {
+                 		 width: 0; 
+                 		 height:0; 
+		 		 position:relative;
+		 		 display:inline-block;
+                 		 border-left: 320px solid transparent;
+                  		 border-right: 320px solid transparent;
+                  		 border-top: 560px solid #f00;
+			}
+			.arrows{
+				margin-left:0px;
+				margin-right:-319px;
+			}
+			.hexagon{
+				position:absolute;
+				height:1200px;
+				width:1200px;
+				border:5px solid #FF0000;
+			}
 
-              .arrow-right {
-                  width: 0; 
-                  height: 0; 
-                  border-top: 60px solid transparent;
-                  border-bottom: 60px solid transparent;
-                  border-left: 60px solid green;
-              }
+	      		#divTest{
+				position:relative;
+				height: 300px;
+				border: 3px solid #736294;
+				width:300px;
+				background-color: #00FFFF;
+				color: #00FFFF;
+				left: 20px;
+				display: inline-block;
+				opacity: 1;
+	      		}	
+              		.arrow-right {
+                	 	 width: 0; 
+                	 	 height: 0; 
+                  		 border-top: 60px solid transparent;
+                  		 border-bottom: 60px solid transparent;
+                  		 border-left: 60px solid green;
+              		}
 
-              .arrow-left {
-                  width: 0; 
-                  height: 0; 
-                  border-top: 10px solid transparent;
-                  border-bottom: 10px solid transparent; 
-                  border-right:10px solid blue; 
-              }
-      </style>
-   </head>
-   <body>
-        <div class="arrow-up"></div>
-        <div class="arrow-down"></div>
-        <div class="arrow-left"></div>
-        <div class="arrow-right"></div>
-   </body>
+              		.arrow-left {
+                  		width: 0; 
+                  		height: 0; 
+                  		border-top: 10px solid transparent;
+                  		border-bottom: 10px solid transparent; 
+                  		border-right:10px solid blue; 
+	      		}
+      		</style>
+   	</head>
+	<body>
+		<div class = "hexagon">	
+        		<div class="arrow-up arrows"></div>
+        		<div class="arrow-down arrows"></div>
+        		<div class="arrow-up arrows"></div>
+        		<div class="arrow-down arrows"></div>
+        		<div class="arrow-up arrows"></div>
+			<div class="arrow-down arrows"></div>
+		</div>
+   	</body>
 </html>
