563d994a4398877d591c8e5c43a05e7edf20b960
DanielDunevant <daniel.john.dunevant@gmail.com>
2018-07-23 09:05:56 -0500

    Sending to App Engine to test Mobile Eventually

---
+++ spanTri.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel = "stylesheet" type = "text/css"href="css/spanTri.css">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   	</head>
+	<body>
+		<?php
+			print"<div class =\"span-outer\">\n";
+			print"<span class =\"text\">l;ksd  ;kjs d f;j ;oj so;rij ;ojs ;dlkjosjdofj[owi joisj[ofij [pio oekjf oisj [pfj psijdf p jwwekfjo wjef[oij w[epfj ojsofkj kej f[sj [pjspjf [osjepfj wejfsj e[of j[os efoj woef owej fojw eofj  owe fo wepfj wje oj w;elkj fwjeo j fi wijef iwjoefj woejf ojw eofj owj foijw oef oiwj eojfoj woejf owej fo jweo i</span>\n";
+			print"</div>\n";
+			print"<span class =\"span-outer2\">\n";
+			print"</span>\n";
+			print"<span class =\"text2\">THis is text in this new span element</span>\n";
+		?>
+   	</body>
+</html>
