833f791691fe82f721ffab768644aa2c87938c7e
DanielDunevant <daniel.john.dunevant@gmail.com>
2018-07-24 12:23:57 -0500

    Lock added

--- lock.php
+++ lock.php
@@ -3,14 +3,14 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<meta charset="UTF-8" />
-	<meta name="author" content="Novay Technologies" />
-	<title>Novay Technologies</title>
+	<meta name="author" content="DJD.io" />
+	<title>DJD.io</title>
 	<link rel="shortcut icon" href="/favicon.ico" />
 	<link rel="icon" type="image/png" href="/favicon.png">
 </head>
 <body>
-	<h1 style="text-align:center;">Novay Technologies</h1>
+	<h1 style="text-align:center;">DJD.io</h1>
 	<h2 style="text-align:center;">This site is currently under development</h2>
-	<p style="text-align:center;">If you need to contact us, feel free to do so at: support@novaytechnologies.com</p>
+	<p style="text-align:center;">If you need to contact me, feel free to do so at: daniel.john.dunevant@gmail.com</p>
 </body>
 </html>
