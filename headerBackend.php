c8ac048241e3b3ff018606079b42597307f4c654
DanielDunevant <Daniel.John.Dunevant@gmail.com>
2018-08-20 13:00:30 -0500

    Mobile Friendly

--- headerBackend.php
+++ headerBackend.php
@@ -5,7 +5,7 @@
 <?php
 print($_GET['page']);
 if($_GET['page']=="hexagonTest2-1")
-{print"<link rel=\"stylesheet\" type=\"text/css\" href=\"css/styleshexagon2.css\" />\n";}
+{print"<link rel=\"stylesheet\" type=\"text/css\" href=\"css/styleshexagon.css\" />\n";}
 ?>
  <!--Latest Minified Version of boostrap -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
