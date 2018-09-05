8b4a199160895b2fbad89347e8ec8aab0b156350
DanielDunevant <Daniel.John.Dunevant@gmail.com>
2018-09-05 15:51:22 -0500

    Creating  transition.scss file

---
+++ imageInTri.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <link rel = "stylesheet" type = "text/css"href="css/triDivTest.css">
+                <!-- Latest compiled and minified CSS -->
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                <!-- jQuery library -->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                <!-- Latest compiled JavaScript -->
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        </head>
+	<body>
+<div class='container'>
+  <div class='wrap'>
+    <div class='crop'>
+      <img src='https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg'>
+    </div>
+  </div>
+  <div class='wrap'>
+    <div class='crop'>
+      <img src='https://s3.amazonaws.com/uifaces/faces/twitter/sindresorhus/128.jpg'>
+    </div>
+  </div>
+  <div class='wrap'>
+    <div class='crop'>
+      <img src='https://s3.amazonaws.com/uifaces/faces/twitter/tomaslau/128.jpg'>
+    </div>
+  </div>
+  <div class='wrap'>
+    <div class='crop'>
+      <img src='https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg'>
+    </div>
+  </div>
+  <div class='wrap'>
+    <div class='crop'>
+      <img src='https://s3.amazonaws.com/uifaces/faces/twitter/celikovic/128.jpg'>
+    </div>
+  </div>
+  <div class='wrap'>
+    <div class='crop'>
+      <img src='https://s3.amazonaws.com/uifaces/faces/twitter/pixeliris/128.jpg'>
+    </div>
+  </div>
+  <div class='wrap'>
+    <div class='crop'>
+      <img src='https://s3.amazonaws.com/uifaces/faces/twitter/ok/128.jpg'>
+    </div>
+  </div>
+  <div class='wrap'>
+    <div class='crop'>
+      <img src='https://s3.amazonaws.com/uifaces/faces/twitter/geeftvorm/128.jpg'>
+    </div>
+  </div>
+</div>
+
+	</body>
+</html>
